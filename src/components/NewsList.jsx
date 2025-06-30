2f757172c6b6961316ea803c85f199433112ba80
Abhinaya292 <abhinaya.mukkala@gmail.com>
2025-06-30 14:42:24 +0530

    news_app

---
+++ src/components/NewsList.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useEffect,useState } from 'react';
+import NewsCard from './NewsCard';
+
+let API_KEY = import.meta.env.VITE_API_KEY;
+let url = `https://gnews.io/api/v4/search?q=example&apikey=${API_KEY}`;
+
+
+import.meta.env.VITE_API_KEY
+
+const NewsList = () => {
+    let [news,setNews] = useState([]);
+    
+    useEffect(()=>{
+        const fetchdata=async()=>{
+            await fetch(url)
+            .then((res)=>res.json())
+            .then((da)=>{
+                setNews(da?.articles);
+                localStorage.setItem("news",JSON.stringify(da?.articles));
+        })
+        }
+
+        fetchdata();
+        
+
+
+        // localStorage.setItem("news",JSON.stringify(news));
+        
+    },[])
+
+  return (
+    <div>
+      <div className="row gy-2">
+        {
+            news.map((val)=>
+               <div className="col-sm-12 col-md-4 col-lg-4"  >
+                 <NewsCard title={val.title} des={val.description} imgurl={val.image}/>
+
+               </div>
+            )
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default NewsList
